b1475c7588709b4d099fd437f3efbaa41f7ce27d
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-05-21 19:18:19 +0530

    basics express commands

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require("http");
+const express = require("express");
 
-const routes = require("./routes.js");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
