921654605a063b324f0916e67e7ed978064eee11
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-05-22 18:23:36 +0530

    add express static middleware to serve static files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const bodyParser = require("body-parser");
 
@@ -9,11 +10,15 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//used for passes a staic directory to use any where in file
+//like using css from public folder in any where
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found!</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
