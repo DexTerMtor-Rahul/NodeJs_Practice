d17e425d042d07dfca831da3036e6dc94c619182
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-05-24 14:28:40 +0530

    Wrapping with template engines like pug, ejs, handlebars, etc.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const bodyParser = require('body-parser');
-
+// const expressHbs = require('express-handlebars');
 const app = express();
 
 //this step allows to call pug file from any where
 //by giving the directory globally
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -23,7 +23,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404 Error' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,11 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
 });
 
 module.exports = router;
