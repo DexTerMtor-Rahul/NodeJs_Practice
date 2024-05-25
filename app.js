167b35f76378868ab703875290fb13efbc2f7eaa
DexTerMtor <dextermtor.rahulraj@gmail.com>
2024-05-25 13:27:34 +0530

    added controller functionlity

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
 const app = express();
@@ -10,8 +9,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,11 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //like using css from public folder in any where
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.pageNotfound);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,8 @@
-const path = require('path');
-
-const rootDir = require('../util/path');
-
 const express = require('express');
 const router = express.Router();
 
-const adminData = require('./admin');
+const productController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-  });
-});
+router.get('/', productController.getProducts);
 
 module.exports = router;
